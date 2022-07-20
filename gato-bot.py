4d2e88d8befd34db2fee12b840145a97a8a45915
64x2 / Xofo <79061462+64x2@users.noreply.github.com>
2022-07-20 17:28:46 +0100

    rewrite beta 1, i've had this for like 3 weeks
    i dont remember how up to date this is rewrite wise, hence the beta 1 name

--- gato-bot.py
+++ gato-bot.py
@@ -1,17 +1,8 @@
-#   /$$$$$$              /$$               /$$$$$$$              /$$     
-#  /$$__  $$            | $$              | $$__  $$            | $$     
-# | $$  \__/  /$$$$$$  /$$$$$$    /$$$$$$ | $$  \ $$  /$$$$$$  /$$$$$$   
-# | $$ /$$$$ |____  $$|_  $$_/   /$$__  $$| $$$$$$$  /$$__  $$|_  $$_/   
-# | $$|_  $$  /$$$$$$$  | $$    | $$  \ $$| $$__  $$| $$  \ $$  | $$     
-# | $$  \ $$ /$$__  $$  | $$ /$$| $$  | $$| $$  \ $$| $$  | $$  | $$ /$$ 
-# |  $$$$$$/|  $$$$$$$  |  $$$$/|  $$$$$$/| $$$$$$$/|  $$$$$$/  |  $$$$/  
-#  \______/  \_______/   \___/   \______/ |_______/  \______/    \___/    
-#
-
-# Imports
+import discord
 from discord import Client, Activity, ActivityType, Embed, Member, HTTPException
 from discord.ext import commands
 from datetime import datetime
+from os import system
 
 import asyncio, time, os, requests, random
 
@@ -23,41 +14,34 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 VERSION = os.getenv("VERSION")
-
-purple_dark= 0x6a006a # Colours
-purple_medium= 0xa958a5
-purple_light= 0xc481fb
-orange= 0xffa500
-gold= 0xdaa520
-red_dark= 8e2430
-red_light= 0xf94343
-blue_dark= 0x3b5998
-cyan= 0x5780cd
-blue_light= 0xace9e7
-aqua= 0x33a1ee
-pink= 0xff9dbb
-green_dark= 0x2ac075
-green_light= 0xa1ee33
-white= 0xf9f9f6
-cream= 0xffdab9
+PREFIX = os.getenv("PREFIX")
+STATUS = os.getenv("STATUS")
 
 client = Client() # Grab token from .env, command prefix and activity
 b = commands.Bot(
-    command_prefix = '!',
-    activity = Activity(type=ActivityType.watching, name="commits @ github.com/64x2/gato-bot-public")
+    command_prefix = f'{PREFIX}',
+    activity = Activity(type=ActivityType.playing, name=f'{STATUS}')
 )
+
+b.remove_command("help")
 
 @b.event # Startup Print
 async def on_ready():
     print(
-        """  ________        __        __________        __    \n"""
-        """ /  _____/_____ _/  |_  ____\______   \ _____/  |_  \n"""
-        """/   \  ___\__  \\   __\/  _ \|    |  _//  _ \   __\ \n"""
-        """\    \_\  \/ __ \|  | (  <_> )    |   (  <_> )  |   \n"""
-        """ \______  (____  /__|  \____/|______  /\____/|__|   \n"""
-        """        \/     \/                   \/              \n"""
-        """                                                    \n"""
-        f"""Welcome to GatoBot {VERSION} by Xofo & Invy55. Avaliable at github.com/64x2/gato-bot-public\n"""
+        """   /$$$$$$              /$$               /$$$$$$$              /$$     \n"""
+        """  /$$__  $$            | $$              | $$__  $$            | $$     \n"""
+        """ | $$  \__/  /$$$$$$  /$$$$$$    /$$$$$$ | $$  \ $$  /$$$$$$  /$$$$$$   \n"""
+        """ | $$ /$$$$ |____  $$|_  $$_/   /$$__  $$| $$$$$$$  /$$__  $$|_  $$_/   \n"""
+        """ | $$|_  $$  /$$$$$$$  | $$    | $$  \ $$| $$__  $$| $$  \ $$  | $$     \n"""
+        """ | $$  \ $$ /$$__  $$  | $$ /$$| $$  | $$| $$  \ $$| $$  | $$  | $$ /$$ \n"""
+        """ |  $$$$$$/|  $$$$$$$  |  $$$$/|  $$$$$$/| $$$$$$$/|  $$$$$$/  |  $$$$/ \n"""
+        """  \______/  \_______/   \___/   \______/ |_______/  \______/    \___/   \n"""
+        """                                                                        \n"""
+        f"""Welcome to Gato Bot {VERSION} by Gato Services\n"""
+        f"""MOTD 20/07/22: Beta 1 public release finally!\n"""
+        f"""Logged in as {b.user}\n"""
+        f"""Status is [{STATUS}]\n"""
+        f"""Prefix is [{PREFIX}]\n"""
     )
 
 @b.command(pass_context=True) # Ping/Latency command
@@ -66,27 +50,26 @@ async def ping(ctx):
     await ctx.trigger_typing()
     time_2 = time.perf_counter()
     ping = round((time_2-time_1)*1000)
-    await ctx.send(f"Pong! Gato-bot currently has a ping of {ping}ms")
+    await ctx.send(f"Pong! Gato Bot currently has a ping of {ping}ms")
 
-@b.command() # Grabs current version
+@b.command(aliases=["ver"]) # Grabs current version
 async def version(ctx):
-    embed=Embed(title=f"Gato-bot.py {VERSION}", url="https://github.com/64x2/gato-bot", description="Gato-bot.py is property of Xofo LLC. Unauthorized use and/or abuse of Xofo LLC property isn't nice :(", color=800080)
+    embed=Embed(title=f"Gato Bot {VERSION}", url="https://github.com/gato-services", description="GatoBot is a multi-use discord bot, developed by the Gato Services team!", color=0xc481fb)
     await ctx.send(embed=embed)
-    print("Command Version was used")
 
-@b.command() # Coinflip
+@b.command(aliases=["cf"]) # Coinflip
 async def coinflip(ctx):
     lista = ['head', 'tails']
     coin = random.choice(lista)
     try:
         if coin == 'head':
-            embed= Embed(color= orange, title="Head",timestamp=datetime.utcfromtimestamp(time.time()))
-            embed.set_thumbnail(url="https://webstockreview.net/images/coin-clipart-dime-6.png")
+            embed= Embed(color=0xc481fb, title="Head",timestamp=datetime.utcfromtimestamp(time.time()))
+            embed.set_thumbnail(url="https://www.the-random-generator.com/img/heads.png")
             embed.set_footer(text=f" GatoBot {VERSION}")
             await ctx.send(embed=embed)
         else:
-            embed= Embed(color= orange, title="Tails",timestamp=datetime.utcfromtimestamp(time.time()))
-            embed.set_thumbnail(url="https://www.nicepng.com/png/full/146-1464848_quarter-tail-png-tails-on-a-coin.png")
+            embed= Embed(color=0xc481fb, title="Tails",timestamp=datetime.utcfromtimestamp(time.time()))
+            embed.set_thumbnail(url="https://www.the-random-generator.com/img/tails.png")
             embed.set_footer(text=f" GatoBot {VERSION}")
             await ctx.send(embed=embed)
     except HTTPException:
@@ -105,7 +88,7 @@ async def usdtobtc(ctx, num: int=None):
             pret = int(resp.json()['USD']['last'])
             final = num/pret
             try:
-                embed = Embed(color= orange, title="USD -> BTC", description=f"USD: {num}\n BTC: {final}", timestamp=datetime.utcfromtimestamp(time.time()))
+                embed = Embed(color=0xc481fb, title="USD -> BTC", description=f"USD: {num}\n BTC: {final}", timestamp=datetime.utcfromtimestamp(time.time()))
                 embed.set_thumbnail(url="https://i.imgur.com/GCPDIYU.png")
                 embed.set_footer(text=f" GatoBot {VERSION}")
                 await ctx.send(embed=embed)
@@ -176,16 +159,22 @@ async def leet(ctx, *, message: str=None):
         for l, r in rmap.items():
             leetmsg = leetmsg.replace(l, r)
         try:
-            embed= Embed(color= green_dark, title="1337 Haxor", description=f"{leetmsg.upper()}", timestamp=datetime.utcfromtimestamp(time.time()))
-            embed.set_thumbnail(url="https://i.imgur.com/TR2cv3C.jpg")
+            embed= Embed(color=0xc481fb, title="1337 Haxor", description=f"{leetmsg.upper()}", timestamp=datetime.utcfromtimestamp(time.time()))
             embed.set_footer(text=f" GatoBot {VERSION}")
             await ctx.send(embed=embed)
         except HTTPException:
             await ctx.send(f"{leetmsg.upper()}")
-            
-@b.command() # Free Nitro !!!!!!
-@commands.has_permissions(mention_everyone=True)
-async def nitro(ctx):
-    await ctx.send("@everyone Free Discord Nitro AirDrop from Steam! https://steams-discord.ru/airdrop")
+
+@b.command() # help men
+async def help(ctx):
+    embed=Embed(title=f"GatoBot Commands • {VERSION}", url="https://github.com/gato-services", description="**avatar:** grabs the avatar of a selected user\n **ban:** ban a selected user\n **coinflip/cf:** flip a coin\n **help:** shows this page\n **kick:** kick a selected user\n **leet:** H4[K$ 73$7\n **ping:** gets the delay in ms\n **unban:** unbans a selected user\n **usdtobtc:** watch btc drop\n **version/ver:** gatobot version\n", color=0xc481fb)
+    embed.set_thumbnail(url="https://avatars.githubusercontent.com/u/107760692?s=400&u=881e852ca28eacd526e86e51def5fbfcc47f3752&v=4")
+    await ctx.send(embed=embed)
+
+@b.command()
+async def avatar(ctx, *,  avamember : discord.Member=None):
+    userAvatarUrl = avamember.avatar_url
+    await ctx.send(f"**Avatar of** `{avamember}`")
+    await ctx.send(userAvatarUrl)
 
 b.run(TOKEN) # Run bot init
